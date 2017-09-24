698fa9e92d034aa048dd683eab2a613b920cb8e6
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-09-24 09:03:22 +0000

    finish login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to root_path
     else
       flash.now[:danger] = "入力された情報が間違っています。再度お試しください。"
       render 'new'
