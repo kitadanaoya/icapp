04573885dc0fe33446179623d4799eea2abc186f
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-10-08 17:53:19 +0000

    book done, next activities & donation, questions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,18 +2,18 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar rounded")
   end
   
   def sum_for(user)
       value = user.point_ids.inject(:+)
       case value
-      when 1..2
-        "これこれをしたらいいさ"
-      when 3..9
-        "あれあれをしたらいいさ"
-      when 10..20
-        "それそれをしたらいいさ"
+      when 1..5
+        render 'shared/15'
+      when 6..18
+        render 'shared/618'
+      when 19..30
+        render 'shared/1930'
       end
   end
 end
