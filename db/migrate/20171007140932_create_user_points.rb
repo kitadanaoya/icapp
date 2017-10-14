5e3d8dd69d883d7815fef8ff85c1cf46b8f5a37d
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-10-14 17:37:53 +0000

    for heroku

--- db/migrate/20171007140932_create_user_points.rb
+++ db/migrate/20171007140932_create_user_points.rb
@@ -1,7 +1,7 @@
 class CreateUserPoints < ActiveRecord::Migration[5.1]
   def change
     create_table :user_points do |t|
-      t.references :user, foreign_key: true
+      t.references :user, :column => :point_ids , foreign_key: true
       t.references :point, foreign_key: true
 
       t.timestamps
