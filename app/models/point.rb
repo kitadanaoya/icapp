22790bd47143f29fa8b17a5bfb95e4ceee82bf03
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-10-14 18:26:53 +0000

    for heroku

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :tokuten, presence: true, numericality: :only_integer
-  has_many :user_points, :column => :point_ids, dependent: :destroy
+  has_many :user_points, dependent: :destroy
   has_many :users, through: :user_points
 end
--- db/migrate/20171013153002_add_point_ids_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPointIdsToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :point_ids, :integer
-  end
-end
